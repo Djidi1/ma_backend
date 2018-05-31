dd0ff2d4647c4d540cc4dc49bb47b49020f0a13f
Djidi1 <djidi@mail.ru>
2018-05-31 22:25:09 +0300

    fix dubl declare

--- database/migrations/2018_03_11_123355_alter_audit_objects_table.php
+++ database/migrations/2018_03_11_123355_alter_audit_objects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterAuditObjectsTable extends Migration
+class AlterAuditObjectsTable_remove extends Migration
 {
     /**
      * Run the migrations.
