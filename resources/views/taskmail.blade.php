b266fcc7c919d29d70ffdc94426e211c3b3ced22
Daniil <djidi@mail.ru>
2018-07-30 10:12:00 +0300

    fix break

--- resources/views/taskmail.blade.php
+++ resources/views/taskmail.blade.php
@@ -9,7 +9,7 @@
     • Номер задачи: <b>{{ $task_id }}</b><br/>
     • Назначено: <b>{{ $user['name'] }}</b><br/>
     • Дата выполнения: <b>{{ $end_date }}</b><br/>
-    • Описание: <b>{{ $comment }}</b>
+    • Описание: <b>{{ $comment }}</b><br/>
     • Задача: <a href="{{ env('APP_URL') }}/#/tasks_list/{{ $task_id }}">устранение несоответствия №{{ $task_id }}</a>
 </body>
 </html>
