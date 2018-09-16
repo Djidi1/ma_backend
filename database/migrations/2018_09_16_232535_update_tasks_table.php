8bc17360c1395157569ac9c1f28028fbe5823b87
djidi1 <djidi@mail.ru>
2018-09-16 23:29:57 +0300

    new field in tasks table

---
+++ database/migrations/2018_09_16_232535_update_tasks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function($table) {
+            $table->integer('responsible_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function($table) {
+            $table->dropColumn('responsible_id');
+        });
+    }
+}
